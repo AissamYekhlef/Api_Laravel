0fd56455c535c6d128a6d7a315e9cbbc4fb6910b
issam <issamyayou@gmail.com>
2020-11-28 18:50:52 +0100

    fix arabic search error 2

--- app/Http/Controllers/TranslateController.php
+++ app/Http/Controllers/TranslateController.php
@@ -174,7 +174,7 @@ class TranslateController extends Controller
 
         $term =  $request->term;
         if(!preg_match('/\p{Arabic}/u', $term)){
-            str_replace(' ', '_', $term); // to make the term searched to snake case 
+            $term = str_replace(' ', '_', $term); // to make the term searched to snake case 
             $term = strtoupper($term); // and make it 
         }
         $results =  Translate::where('wEN', 'LIKE', '%'. $term . '%')
